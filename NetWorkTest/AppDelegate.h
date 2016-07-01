d9c91ab9461fd71496923fdec518171f6bd25e2b
Ethan He <tashaxing123@163.com>
2016-07-01 11:03:15 +0800

    new

---
+++ NetWorkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWorkTest
+//
+//  Created by yxhe on 16/7/1.
+//  Copyright © 2016年 yxhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetWorkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetWorkTest
+//
+//  Created by yxhe on 16/7/1.
+//  Copyright © 2016年 yxhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
